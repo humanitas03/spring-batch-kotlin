df316f7432721d021f6a63fd7131cfafd3afe34d
humanitas03 <dlswp113@gmail.com>
2021-10-22 19:05:17 +0900

    Initial commit

---
+++ src/main/kotlin/com/example/springbatchkotlin/SpringBatchKotlinApplication.kt
@@ -0,0 +1,13 @@
+package com.example.springbatchkotlin
+
+import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+@EnableBatchProcessing
+class SpringBatchKotlinApplication
+
+fun main(args: Array<String>) {
+    runApplication<SpringBatchKotlinApplication>(*args)
+}
---
+++ src/test/kotlin/com/example/springbatchkotlin/SpringBatchKotlinApplicationTests.kt
@@ -0,0 +1,12 @@
+package com.example.springbatchkotlin
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class SpringBatchKotlinApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+}
